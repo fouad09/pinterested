e32dc47c2401db6467558c5cf7908f9035737825
fouad09 <fouadkouidmir@gmail.com>
2016-06-15 16:52:03 +0000

    tweaked bootstrap ui

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = {host:'pinterested0909.herokuapp.com'}
+  
   confi.paperclip_default = {
     :storage => :s3,
     :s3_credentials =>{
